7380db8b6b7dc9b2fda22c853049712564771cdd
karlZh <2219619335@qq.com>
2016-03-08 23:50:03 +0800

    登陆表单

--- protected/controllers/SalemanController.php
+++ protected/controllers/SalemanController.php
@@ -10,6 +10,10 @@ class SalemanController extends Controller{
      * 业务员登陆页面
      */
     public function actionLogin(){
+        $model = new Saleman;
+        if(!empty($_POST['Saleman'])){
+
+        }
         $this->renderPartial('login');
     }
 }
